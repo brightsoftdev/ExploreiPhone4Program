7b3ef546ad3cab35d63588c858b5446a3d3ed568
Rock <wenshyansu@yahoo.com.tw>
2011-11-19 00:52:44 +0800

    新增ch04範例

---
+++ ch04/ControllFun/ControllFun/ControllFunAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ControllFunAppDelegate.h
+//  ControllFun
+//
+//  Created by SU,WEN HSIEN on 11/11/18.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ControllFunViewController;
+
+@interface ControllFunAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet ControllFunViewController *viewController;
+
+@end
---
+++ ch04/ControllFun/ControllFun/ControllFunViewController.h
@@ -0,0 +1,13 @@
+//
+//  ControllFunViewController.h
+//  ControllFun
+//
+//  Created by SU,WEN HSIEN on 11/11/18.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControllFunViewController : UIViewController
+
+@end
